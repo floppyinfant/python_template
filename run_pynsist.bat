7ede103878be1a782396425e418876f66868fa29
Thorsten <transcendenz@web.de>
2018-08-30 17:14:34 +0200

    pynsist

---
+++ run_pynsist.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+
+python -m nsist installer.cfg
+
+REM required files:
+REM installer.cfg
+REM
+REM run in terminal:
+REM pynsist installer.cfg
+REM or run as python module:
+REM python -m nsist installer.cfg

33fb9db133b21aefd8581ca7529c7fe6bf29b6ba
floppyinfant <make@floppyinfant.com>
2025-08-03 16:01:23 +0200

    iterative cached fibonacchi without limit

--- src/hpc/hpc.py
+++ src/hpc/hpc.py
@@ -14,6 +14,8 @@ Solution:
 Dynamic programming
 @cache
 https://docs.python.org/3/library/functools.html#functools.cache
+@lru_cache(maxsize=None)  # Least Recently Used" (LRU)-Cache-Strategie
+https://docs.python.org/3/library/functools.html#functools.lru_cache
 
 2)
 Concurrency
@@ -51,7 +53,7 @@ Taichi
 https://www.taichi-lang.org/ 
 """
 
-from functools import cache
+from functools import cache, lru_cache
 
 #from numba import njit
 #import taichi as ti
@@ -66,6 +68,7 @@ import logging
 logger = logging.getLogger("template")
 
 import time
+#import asyncio
 
 
 def delta_time(f):
@@ -96,6 +99,22 @@ def fibonacci(n: int) -> int:
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
+def fibonacci_iterative(n: int) -> int:
+    """Calculate the nth Fibonacci number using an iterative approach with caching.
+    Args:
+        n (int): The position in the Fibonacci sequence."""
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer.")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+
 @cache
 def fibonacci_cached(n: int) -> int:
     """Calculate the nth Fibonacci number using recursion with caching.
@@ -108,6 +127,23 @@ def fibonacci_cached(n: int) -> int:
     if n == 1:
         return 1
     return fibonacci_cached(n - 1) + fibonacci_cached(n - 2)
+
+
+@cache
+def fibonacci_iterative_cached(n: int) -> int:
+    """Calculate the nth Fibonacci number using an iterative approach with caching.
+    Args:
+        n (int): The position in the Fibonacci sequence."""
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer.")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
 
 
 #@njit(nopython=True)
@@ -145,23 +181,43 @@ def fibonacci_taichi(n: int) -> int:
 
 def main():
     """Main function to demonstrate the Fibonacci calculation."""
-    n = 33
-    
-    # n = 300 done in 0.001 seconds
-    # n = 499 done in 0.001 seconds
-    # n = 500 RecursionError: maximum recursion depth exceeded
-    start_time = time.time()
-    print(f"Fibonacci({n}) with caching: \t{fibonacci_cached(n)} (in {(time.time() - start_time):.6f} seconds)")
+    n = 33     # non-cached recursive solution
+    #n = 499    # max rekursion depth for cached recursive solution
+    #n = 10000  # iterative solution doesn't need to be cached and is the => fastest solution
+    #n = 100000 # ValueError: Exceeds the limit (4300) for integer string conversion; use sys.set_int_max_str_digits() to increase the limit
 
+
+    # recursion, no-cache ----------------------
     # n =  36 done in 5 seconds
     # n =  37 done in 8 seconds
     # n =  38 done in 13 seconds
     start_time = time.time()
-    print(f"Fibonacci({n}) without caching: \t{fibonacci(n)} (in {(time.time() - start_time):.6f} seconds)")
+    print(f"Fibonacci({n}) recursive without caching: \t{fibonacci(n)} (in {(time.time() - start_time):.6f} seconds)")
 
+    # iterative, no-cache ----------------------
+    # n = 5000 done in 0.001 seconds
+    start_time = time.time()
+    print(f"Fibonacci({n}) iterative without caching: \t{fibonacci_iterative(n)} (in {(time.time() - start_time):.6f} seconds)")
+
+    # recursion, cached ------------------------
+    # n = 300 done in 0.001 seconds
+    # n = 499 done in 0.001 seconds
+    # n = 500 RecursionError: maximum recursion depth exceeded
+    # https://stackoverflow.com/questions/74296005/recursion-error-while-trying-to-write-an-memoization-decorator
+    # use for-loop instaed of recursion
+    start_time = time.time()
+    print(f"Fibonacci({n}) recursive with    caching: \t{fibonacci_cached(n)} (in {(time.time() - start_time):.6f} seconds)")
+
+    # iterative, cached ------------------------
+    # n = 5000 done in 0.001 seconds
+    start_time = time.time()
+    print(f"Fibonacci({n}) iterative with    caching: \t{fibonacci_iterative_cached(n)} (in {(time.time() - start_time):.6f} seconds)")
+
+    # numba ------------------------------------
     start_time = time.time()
     #print(f"Fibonacci({n}) with Numba: \t{fibonacci_numba(n)} (in {(time.time() - start_time):.6f} seconds)")
 
+    # taichi -----------------------------------
     #ti.init()
     start_time = time.time()
     #print(f"Fibonacci({n}) with Taichi: \t{fibonacci_taichi(n)} (in {(time.time() - start_time):.6f} seconds)")
